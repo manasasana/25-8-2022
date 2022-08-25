34300f6b9e5a5ea0ffd585739e207b03d10ed033
manasasana <72486825+manasasana@users.noreply.github.com>
2022-08-25 21:19:38 +0530

    Create Assignment.sql

---
+++ Assignment.sql
@@ -0,0 +1,23 @@
+create or alter  function highamt()
+returns @BankDetails table  
+(AccNo int,OpenDate date, LastTransactionDate date) as
+begin  
+    insert into @BankDetails
+	select a.AccNo,a.Cid,a.Aadhar,b.OpenDate,b.LastTransactionDate from AccInfo a inner join BankDetails b 
+	on a.AccNo=B.AccNo where a.Amt>200000;
+return  
+end; 
+
+select * from highamt()
+
+
+create or alter  function show(@cat varchar(10))
+returns @MovieInfo table  
+(MovieName varchar(19),Category varchar(20)) as
+begin  
+    insert into @MovieInfo
+	select MovieName,Category from MovieInfo where Category=@cat;
+	return;
+end;
+
+select * from show('horror')
